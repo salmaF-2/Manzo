27296310e91dcfcb66a440d2303128ddb280a5ca
YounessR01 <rouyouness@gmail.com>
2025-06-26 14:04:07 +0100

    city model edited

--- backend/models/City.js
+++ backend/models/City.js
@@ -1,14 +1,1 @@
-const mongoose = require('mongoose');
 //
-const citySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true }, 
-  coordinates: {
-    lat: Number,
-    lng: Number,
-    zoom: Number
-  },
-  prestataires: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-});
-
-module.exports = mongoose.model('City', citySchema);
